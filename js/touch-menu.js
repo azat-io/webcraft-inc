3704719533df46518b070676386fe14c877adc2b
klevec <azatklevec@gmail.com>
2015-03-28 03:31:41 +0300

    Project is started to develop

---
+++ js/touch-menu.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var a=$("#touch-menu"),b=$("nav");$(a).on("click",function(a){a.preventDefault(),b.slideToggle()}),$(window).resize(function(){var a=$(window).width();a>760&&b.is(":hidden")&&b.removeAttr("style")})});
